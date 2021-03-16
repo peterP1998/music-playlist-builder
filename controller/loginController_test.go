19c0f36a1d283376be7ca24fecee654e8be4b2fe
peterP1998 <49306695+peterP1998@users.noreply.github.com>
2021-03-16 23:05:49 +0200

    add tests

--- controller/loginController_test.go
+++ controller/loginController_test.go
@@ -1,16 +1,52 @@
 package controller
 
 import (
+	"bytes"
 	"github.com/gin-gonic/gin"
-	//"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"
+	//"fmt"
+	"github.com/peterP1998/music-playlist-builder/service"
+	"github.com/peterP1998/music-playlist-builder/service/mocks"
+	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
-func TestLogin(t *testing.T) {
-	resp := httptest.NewRecorder()
-	var loginController LoginController = LoginController{}
+func TestLoginController(t *testing.T) {
+	userRepository:=mocks.UserRepositoryMock{}
+	userService:=service.UserServiceCreate(userRepository)
+	loginService:=service.LoginServiceAuth()
+	loginController:=LoginControllerCreate(loginService,userService)
 	gin.SetMode(gin.TestMode)
-	c, _ := gin.CreateTestContext(resp)
-	loginController.Login(c)
+	router := gin.New()
+	router.POST("/login", loginController.Login)
+	router.POST("/register", loginController.Register)
+	t.Run("login with wrong credentials", func(t *testing.T) {
+		resp := httptest.NewRecorder()
+		var jsonStr = []byte(`{"user":"test","password":"test1"}`)
+		req, _ := http.NewRequest("POST", "/login", bytes.NewBuffer(jsonStr))
+		router.ServeHTTP(resp, req)
+		assert.Equal(t, 401, resp.Result().StatusCode)
+	})
+	t.Run("login with correct credentials", func(t *testing.T) {
+		resp := httptest.NewRecorder()
+		var jsonStr = []byte(`{"user":"test1","password":"test"}`)
+		req, _ := http.NewRequest("POST", "/login", bytes.NewBuffer(jsonStr))
+		router.ServeHTTP(resp, req)
+		assert.Equal(t, 200, resp.Result().StatusCode)
+	})
+	t.Run("register with existing user", func(t *testing.T) {
+		resp := httptest.NewRecorder()
+		var jsonStr = []byte(`{"user":"test","password":"test1","email":"test@abv.bg"}`)
+		req, _ := http.NewRequest("POST", "/register", bytes.NewBuffer(jsonStr))
+		router.ServeHTTP(resp, req)
+		assert.Equal(t, 400, resp.Result().StatusCode)
+	})
+	t.Run("register with non existing user", func(t *testing.T) {
+		resp := httptest.NewRecorder()
+		var jsonStr = []byte(`{"user":"test21","password":"test1","email":"test@abv.bg"}`)
+		req, _ := http.NewRequest("POST", "/register", bytes.NewBuffer(jsonStr))
+		router.ServeHTTP(resp, req)
+		assert.Equal(t, 200, resp.Result().StatusCode)
+	})
 }
